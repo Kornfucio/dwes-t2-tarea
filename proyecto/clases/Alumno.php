b7a4f648e81ca694598575bb61e3cecea91f12bb
Kornfucio <sergiperancho@gmail.com>
2025-12-21 13:56:54 +0100

    Correct Alumnos class

--- proyecto/clases/Alumno.php
+++ proyecto/clases/Alumno.php
@@ -7,8 +7,14 @@
 
         //Métodos
 
-        public function __construct(int $id, string $nombre, string $apellidos, string $email) {
-           parent::__construct($id, $nombre, $apellidos, $email);
+        public function __construct(int $id, string $nombre, string $apellidos, string $email, int $edad/*, array $asignaturas, bool $cursoAbonado*/) {
+           $this->id = $id;
+           $this->nombre = $nombre;
+           $this->apellidos = $apellidos;
+           $this->email = $email;
+           $this->edad = $edad;
+           ///$this->asignaturas = $asignaturas;
+           //$this->cursoAbonado = $cursoAbonado;
         }
         public function abonarCurso(): void {   
             $this->cursoAbonado = true;
@@ -23,9 +29,16 @@
         
         public static function crearAlumnosDeMuestra() {
             return [
-                   new Alumno(1, "Ana", "Curevo", "acurev@cole.com", ),
-                   new Alumno(2, "Pablo","Fruela", "pfruela@cole.com"),
-                   new Alumno(3, "Oli", "Braña", "obraña@cole.com")
+                   new Alumno(1, "Laura", "Martinez", "laura.martinez@email.com", 22),
+                   new Alumno(2, "Sergio","Lopez", "sergio.lopez@email.com", 25),
+                   new Alumno(3, "Carlos", "García", "carlos.garcia@email.com",24),
+                   new Alumno(4, "Marta", "Sánchez", "marta.sanchez@email.com",23),
+                   new Alumno(5, "Alba", "Fernández", "alba.fernandez@email.com",21),
+                   new Alumno(6, "David", "Rodriguez", "david.rodriguez@email.com",26),
+                   new Alumno(7, "Lucía", "Jimenez", "lucia.jimenez@email.com",20),
+                   new Alumno(8, "Jorge", "Perez", "jorge.perez@email.com",22),
+                   new Alumno(9, "Elena", "Romero", "elena.romero@email.com",23),
+                   new Alumno(10, "Pablo", "Torres", "pablo.torres@email.com",24)
             ];
             
         }
